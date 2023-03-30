41790d82dbd70b291e8b19d2cbf32ae90db83cc8
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-03-30 14:33:18 +0530

    create Videos

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,20 +1,41 @@
-import { useState, useEffect } from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from '../Sidebar/Sidebar';
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Sidebar, Videos } from "../";
 
 const Feed = () => {
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row"} }}>
-      <Box sx={{ height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2} }}>
-       <Sidebar />
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
           Copyright 2022 JSM Media
         </Typography>
-
       </Box>
-      
-    </Stack>
-  )
-}
 
-export default Feed
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,5 @@ export { default as ChannelDetail } from './ChannelDetail/ChannelDetail';
 export { default as SearchFeed } from './SearchFeed/SearchFeed';
 export { default as SearchBar } from './SearchBar/SearchBar';
 export { default as Sidebar } from './Sidebar/Sidebar';
+export { default as Videos } from './Videos/Videos';
 
