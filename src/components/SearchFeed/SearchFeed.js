31e8d3bd6434f6739dcfb9e1a14add0993fa7823
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-04-01 14:48:08 +0530

    create functionality for searchFeed

--- src/components/SearchFeed/SearchFeed.js
+++ src/components/SearchFeed/SearchFeed.js
@@ -18,8 +18,7 @@ const SearchFeed = () => {
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        Search Results for: 
-        <span style={{ color: "#F31503" }}>{searchTerm}</span> videos
+        Search Results for: <span style={{ color: "#F31503" }}>{searchTerm}</span> videos
       </Typography>
 
       <Videos videos={videos} />
